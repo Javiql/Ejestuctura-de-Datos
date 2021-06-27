7684fe34533e96dfdba1d456ed394c7668e4f623
Javiql <85724127+Javiql@users.noreply.github.com>
2021-06-27 07:30:46 -0500

    Add files via upload

---
+++ Tarea S1- EjerciciosPáginaWeb/Cargo.py
@@ -0,0 +1,12 @@
+class Cargo:
+    def__init__(self):
+        self.codigo=99
+        self.descripcion="Sin Cargo"
+        
+cargo1 = Cargo()
+print(cargo1.codigo,cargo1.descripcion)
+cargo2 = Cargo()
+cargo2.codigo=1
+cargo2.descripcion="Docente"
+print(cargo2.codigo,cargo2.descripcion)
+    
